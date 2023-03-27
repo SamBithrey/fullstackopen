da59c65ff3e44c706f54ce8a825b4250251b5215
Sam Bithrey <sambithrey@gmail.com>
2023-03-27 16:01:54 +0100

    FSO Part 2.16-2.17

--- part2/phonebook/src/Components/Numbers.js
+++ part2/phonebook/src/Components/Numbers.js
@@ -1,10 +1,11 @@
 const Numbers = ({people, deleteContact}) => {
     return (
         people.map((p) => (
-        <div key={p.id}>
+        <div key={p.id} className='contact'>
             <span>Name: {p.name}</span><br/>
             <span>Number: {p.number}</span><br/>
             <button onClick={() => deleteContact(p.id)}>Delete?</button><br/>
+            <br/>
         </div> 
         ))
     )

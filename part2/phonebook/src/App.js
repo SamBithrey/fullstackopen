aac8b7f66e8f4cf3346190632eccdbefc0dac132
Sam Bithrey <sambithrey@gmail.com>
2023-03-27 10:59:49 +0100

    FSO Part 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Numbers from './Components/Numbers'
 import Filter from './Components/Filter'
 import NewPerson from './Components/NewPerson'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  document.title = `Phonebook`;
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [showFilter, setShowFilter] = useState(false)
-  
+
+  const hook = () => {
+    //console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        //console.log('promise fulfilled')
+        setPersons(res.data)
+      })
+  }
+
+  useEffect(hook, [])
+
   const setToPersons = (event) => {
     event.preventDefault()
     if (newNumber === '') {

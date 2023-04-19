348ab9a012f94b58fe2a8359aec888f73760c5c9
Sam Bithrey <sambithrey@gmail.com>
2023-04-19 17:21:02 +0100

    FSO Part 9.1-9.3

---
+++ part9/first-steps-with-typescript/bmiCalculator.ts
@@ -0,0 +1,44 @@
+interface BMIValues {
+    heightInCM: number;
+    weightInKG: number;
+}
+  
+const bmiArguments = (args: string[]): BMIValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+  
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            heightInCM: Number(args[2]),
+            weightInKG: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+const calculateBmi = (heightInCM: number, weightInKG: number) : string => {
+    const BMI = weightInKG/(Math.pow(heightInCM/100, 2))
+
+    switch ( true ) {
+        case ( BMI < 18.5 ):
+            return "Underweight"
+        case ( BMI >= 18.5 && BMI < 25):
+            return "Normal Weight"
+        case ( BMI >= 25 && BMI < 30):
+            return "Overweight"
+        case (BMI >= 30):
+            return "Obese"
+    }
+}
+
+try {
+    const { heightInCM, weightInKG } = bmiArguments(process.argv);
+    console.log(calculateBmi(heightInCM, weightInKG));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
---
+++ part9/first-steps-with-typescript/exerciseCalculator.ts
@@ -0,0 +1,75 @@
+interface ExerciseValues {
+    exerciseHours: number[];
+    targetHours: number;
+}
+  
+const exerciseArguments = (args: string[]): ExerciseValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    const targetHours = Number(args[2])
+    const removedArgs = args.splice(0, 3)
+    const exerciseHours: number[] = args.map((hours) => Number(hours));
+
+    if (!isNaN(Number(args[2])) && !exerciseHours.includes(NaN)) {
+        return {
+            exerciseHours: exerciseHours,
+            targetHours: targetHours
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+
+interface exerciseResults {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+const calculateExercises = (exerciseHours: number[], targetHours: number) : exerciseResults => {
+    let result = {
+        periodLength: 0,
+        trainingDays: 0,
+        success: false,
+        rating: 0,
+        ratingDescription: "",
+        target: 0,
+        average: 0
+    }
+    result.target = targetHours
+    result.periodLength = exerciseHours.length
+    result.trainingDays = exerciseHours.filter((hours) => hours > 0).length
+    result.average = (exerciseHours.reduce(function(a: number, b: number){return a + b;}))/exerciseHours.length
+    if(result.average > result.target) {result.success = true}
+    switch (true) {
+        case (result.average === 0):
+            result.rating = 1
+            result.ratingDescription = 'You didn\'t train at all!!!'
+            break;
+        case (result.success === false):
+            result.rating = 2
+            result.ratingDescription = 'Not too bad but could be better'
+            break;
+        case (result.success === true):
+            result.rating = 3
+            result.ratingDescription = 'You smashed it this time!'
+            break;
+    }
+    return result
+}
+
+
+try {
+    const { exerciseHours, targetHours } = exerciseArguments(process.argv);
+    console.log(calculateExercises(exerciseHours, targetHours));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
